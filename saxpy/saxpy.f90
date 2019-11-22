aa87300443c47949ff70c9f2a4da3f83e88d5280
clementval <clementval@gmail.com>
2019-11-22 11:48:32 -0500

    Push a small saxpy example f90 + mlir

---
+++ saxpy/saxpy.f90
@@ -0,0 +1,27 @@
+
+program main
+  implicit none
+  integer, parameter :: n = 1024
+  real :: x(n), y(n), a = 10.0
+
+  x(:) = 1.0
+  y(:) = 2.0
+
+
+  call saxpy(x,y,n,a)
+  print*,y(:)
+
+contains
+
+  subroutine saxpy(x,y,n,a)
+    implicit none
+    real :: a, x(n), y(n)
+    integer :: n, i
+    !$ acc kernels
+    do i = 1, n
+      y(i) = a*x(i)+y(i)
+    enddo
+    !$ acc end kernels
+  end subroutine saxpy
+
+end program
